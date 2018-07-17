f7a3ce3137f7e420253524c3b957298e02389447
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-17 17:26:33 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,25 @@
 
 def key_for_min_value(name_hash)
 
+ 
+ comp_value = [ ]
+ 
+ # collect keys
+ keys = name_hash.collect do |key, value|
+   key
+  end
+ 
+ 
+ comp_value = name_hash[keys[0]]
+ lowest_key = keys[0]
+   name_hash.collect do |key, value|
+      if value <= comp_value
+        lowest_key = key
+     end
+   end  
+
+lowest_key
+
 end
+
+
