ba22d4130ca750e89d8a89a2a51e9569866f8c36
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-17 17:25:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,13 +11,14 @@ def key_for_min_value(name_hash)
    key
   end
  comp_value = name_hash[keys[0]]
-
+ lowest_key = keys[0]
 name_hash.collect do |key, value|
   if value <= comp_value
-    key
+    lowest_key = key
   end
 end  
 
+lowest_key
 
 end
 
